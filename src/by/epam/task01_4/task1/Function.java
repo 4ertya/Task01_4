2504f93f93d097c416220dbcab28c1a18786db0c
Dmitry Palchynski <palchmail@gmail.com>
2020-06-05 11:43:31 +0300

    fix task1

--- src/by/epam/task01_4/task1/Function.java
+++ src/by/epam/task01_4/task1/Function.java
@@ -1,44 +1,30 @@
 package by.epam.task01_4.task1;
 
 public class Function {
-    private double a;
-    private double b;
-    private double c;
-    private double result;
+    private final double a = 2;
+    private final double b = 3;
+    private final double c = 4;
+
 
     public double getA() {
         return a;
-    }
-
-    public void setA(double a) {
-        this.a = a;
     }
 
     public double getB() {
         return b;
     }
 
-    public void setB(double b) {
-        this.b = b;
-    }
-
     public double getC() {
         return c;
     }
 
-    public void setC(double c) {
-        this.c = c;
-    }
-
-    public void setResult(double result) {
-        this.result = result;
-    }
-
     public double getResult() {
-        return (b + Math.sqrt(Math.pow(b, 2) + 4 * a * c)) / (2 * a) - Math.pow(a, 3) * c + Math.pow(b, -2);
-    }
+        double numerator;
+        double denominator;
 
-    public void printResult() {
-        System.out.println("Result: " + getResult());
+        numerator = b + Math.sqrt(Math.pow(b, 2) + 4 * a * c);
+        denominator = 2 * a;
+
+        return numerator / denominator - Math.pow(a, 3) * c + Math.pow(b, -2);
     }
 }
